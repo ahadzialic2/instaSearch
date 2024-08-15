b35e6b8f4a2ff065928368767c95726d8b5a6705
Adnan Hadzialic <adnan.hadzialic@outlook.com>
2024-08-15 15:11:34 +0200

    hotfix- js loads with having to reload page (on back button press or link opened in same tab)

--- app/javascript/articles/article_search.js
+++ app/javascript/articles/article_search.js
@@ -1,60 +1,63 @@
-let input = document.getElementById("search-input");
-const spinner = document.getElementById("spinner");
-let searchResults = document.getElementById("search-results");
-const menu = document.getElementById("menu");
-const sidebar = document.getElementById("side-nav");
+document.addEventListener("DOMContentLoaded", function () {
+  let input = document.getElementById("search-input");
+  const spinner = document.getElementById("spinner");
+  let searchResults = document.getElementById("search-results");
+  const menu = document.getElementById("menu");
+  const sidebar = document.getElementById("side-nav");
 
-let typingTimer; // Timer identifier
-const typingDelay = 1500; // Delay in milliseconds (3 seconds)
+  let typingTimer; // Timer identifier
+  const typingDelay = 1500; // Delay in milliseconds (3 seconds)
 
-menu.addEventListener("click", (event) => {
-  sidebar.style.left = sidebar.style.left === "-250px" ? "0" : "-250px";
-});
+  menu.addEventListener("click", (event) => {
+    sidebar.style.left = sidebar.style.left === "-250px" ? "0" : "-250px";
+  });
 
-input.addEventListener("keyup", handleSearchInput);
+  input.addEventListener("keyup", handleSearchInput);
 
-function handleSearchInput(event) {
-  if (event.target.value.length === 0) {
-    clearTimeout(typingTimer);
+  function handleSearchInput(event) {
+    console.log("searching...");
+    if (event.target.value.length === 0) {
+      clearTimeout(typingTimer);
+      searchResults.innerHTML = "";
+      spinner.style.display = "none"; // Hide spinner
+      return;
+    }
+
     searchResults.innerHTML = "";
-    spinner.style.display = "none"; // Hide spinner
-    return;
-  }
+    clearTimeout(typingTimer);
 
-  searchResults.innerHTML = "";
-  clearTimeout(typingTimer);
+    spinner.style.display = "block"; // Show spinner
 
-  spinner.style.display = "block"; // Show spinner
+    let query = event.target.value;
 
-  let query = event.target.value;
+    // Set a new timer
+    typingTimer = setTimeout(() => {
+      if (!query) return;
 
-  // Set a new timer
-  typingTimer = setTimeout(() => {
-    if (!query) return;
+      fetch(`/articles/search?query=${query}`, {
+        headers: {
+          Accept: "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((articles) => {
+          //do somethig with articles
+          let results = articles.results;
+          if (!results.length) {
+            spinner.style.display = "none"; // Hide spinner
 
-    fetch(`/articles/search?query=${query}`, {
-      headers: {
-        Accept: "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((articles) => {
-        //do somethig with articles
-        let results = articles.results;
-        if (!results.length) {
+            searchResults.innerHTML = "No results found";
+            return;
+          }
+          searchResults.innerHTML = "";
+
+          results.forEach((article) => {
+            let li = document.createElement("li");
+            li.textContent = article.name;
+            searchResults.appendChild(li);
+          });
           spinner.style.display = "none"; // Hide spinner
-
-          searchResults.innerHTML = "No results found";
-          return;
-        }
-        searchResults.innerHTML = "";
-
-        results.forEach((article) => {
-          let li = document.createElement("li");
-          li.textContent = article.name;
-          searchResults.appendChild(li);
         });
-        spinner.style.display = "none"; // Hide spinner
-      });
-  }, typingDelay);
-}
+    }, typingDelay);
+  }
+});
